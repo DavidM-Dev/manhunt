6af8ab7bcffc476420b527f9eb9fe41eb74844cf
janakitti <janakittiratana@gmail.com>
2019-09-14 15:49:08 -0400

    Added scoreboard file

--- src/pages/ingame.js
+++ src/pages/ingame.js
@@ -8,7 +8,24 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAsQPZmUAX-vWfxqAjjjW5RrzIyb1mTTIM",
+  authDomain: "manhunt-9d5b4.firebaseapp.com",
+  databaseURL: "https://manhunt-9d5b4.firebaseio.com",
+  projectId: "manhunt-9d5b4",
+  storageBucket: "manhunt-9d5b4.appspot.com",
+  messagingSenderId: "467777965790",
+  appId: "1:467777965790:web:6920d3d120b7ea3d557c4f"
+};
+
 const IndexPage = () => {
+  
+  useEffect(() => {
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();  
+  }, [])
+  
+
   return (
    <>
       <SEO title="MANHUNT"/>
@@ -16,7 +33,7 @@ const IndexPage = () => {
       <div className="App">
         <Camera />
       </div>
-      
+
       <table class="Scoreboard">
         <tr>
           <td>Player</td>
@@ -27,8 +44,27 @@ const IndexPage = () => {
           <td>INFECTED</td>
         </tr>
       </table>
+
+      <table classname="scoreboardTable">
+        <thead>
+          <tr>
+            <th>Player</th>
+            <th>Location</th>
+            <th>Status</th>
+          </tr>
+          <tr>
+            <td><i></i> No result</td>
+          </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+      </table>
+
    </>
   )
 }
+
+
 
 export default IndexPage
